ea27e8407dbd27daf340cb6f22cc766938664145
Omji Kushwaha <omjimaurya09@gmail.com>
2022-10-19 04:28:07 +0530

    feat:added the token data of polygon and homestead

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import { Chain } from './types';
+import polygon from './chains/polygon'; // all the tokens are imported from polygon
+import homestead from './chains/homestead'; //all the tokens are imported form homestead
+
+//all the Chains  are  exported  in format of object
+
+const tokens: Chain = { ...polygon, ...homestead };
+
+// Export token
+export { tokens };
